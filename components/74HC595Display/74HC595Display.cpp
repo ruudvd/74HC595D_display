11aa68cae100f4cb9e875475f256ce4dfe8d075c
Ruud van Dommelen <r.vandommelen@beeliners.com>
2021-12-23 14:03:23 +0100

    moved }

--- components/74HC595Display/74HC595Display.cpp
+++ components/74HC595Display/74HC595Display.cpp
@@ -134,15 +134,16 @@ namespace esphome
                 this->scroll_left();
                 this->display();
             }
+        }
 
-            void LedDisplayComponent::display()
+        void LedDisplayComponent::display()
+        {
+            unsigned int row = 0;
+            for (uint8_t line = 0; line < this->get_height_internal(); line++)
             {
-                unsigned int row = 0;
-                for (uint8_t line = 0; line < this->get_height_internal(); line++)
+                for (uint16_t j = 0; j < this->get_width_internal(); j++)
                 {
-                    for (uint16_t j = 0; j < this->get_width_internal(); j++)
-                    {
-                        /*
+                    /*
                     if (this->reverse_)
                     {
                     }
@@ -150,60 +151,60 @@ namespace esphome
                     {
                     }
                     */
-                        SET_LED(this->led_displaybuffer_[chip_line][j]);
-                    }
-                    row = rows[line];
-                    digitalWrite(LatchClock, true);
-                    delayMicroseconds(10);
-                    digitalWrite(LatchClock, false);
-                    digitalWrite(row, true);
-                    delayMicroseconds(2000); //was 1000
-                    digitalWrite(row, false);
+                    SET_LED(this->led_displaybuffer_[chip_line][j]);
                 }
+                row = rows[line];
+                digitalWrite(LatchClock, true);
+                delayMicroseconds(10);
+                digitalWrite(LatchClock, false);
+                digitalWrite(row, true);
+                delayMicroseconds(2000); //was 1000
+                digitalWrite(row, false);
             }
+        }
 
-            int LedDisplayComponent::get_height_internal()
-            {
-                return this->num_chip_lines_;
-            }
+        int LedDisplayComponent::get_height_internal()
+        {
+            return this->num_chip_lines_;
+        }
 
-            int LedDisplayComponent::get_width_internal()
-            {
-                return this->num_chips_ * 8;
-            }
+        int LedDisplayComponent::get_width_internal()
+        {
+            return this->num_chips_ * 8;
+        }
 
-            void HOT LedDisplayComponent::draw_absolute_pixel_internal(int x, int y, Color color)
-            {
-                if (x + 1 > (int)this->led_displaybuffer_[0].size())
-                { // Extend the display buffer in case required
-                    for (int chip_line = 0; chip_line < this->num_chip_lines_; chip_line++)
-                    {
-                        this->led_displaybuffer_[chip_line].resize(x + 1, this->bckgrnd_);
-                    }
-                }
-
-                if ((y >= this->get_height_internal()) || (y < 0) || (x < 0)) // If pixel is outside display then dont draw
-                    return;
-
-                uint16_t pos = x;   // X is starting at 0 top left
-                uint8_t subpos = y; // Y is starting at 0 top left
-
-                if (color.is_on())
+        void HOT LedDisplayComponent::draw_absolute_pixel_internal(int x, int y, Color color)
+        {
+            if (x + 1 > (int)this->led_displaybuffer_[0].size())
+            { // Extend the display buffer in case required
+                for (int chip_line = 0; chip_line < this->num_chip_lines_; chip_line++)
                 {
-                    this->led_displaybuffer_[subpos / 8][pos] |= (1 << subpos % 8);
-                }
-                else
-                {
-                    this->led_displaybuffer_[subpos / 8][pos] &= ~(1 << subpos % 8);
+                    this->led_displaybuffer_[chip_line].resize(x + 1, this->bckgrnd_);
                 }
             }
 
-            void LedDisplayComponent::send_byte_(uint8_t a_register, uint8_t data)
+            if ((y >= this->get_height_internal()) || (y < 0) || (x < 0)) // If pixel is outside display then dont draw
+                return;
+
+            uint16_t pos = x;   // X is starting at 0 top left
+            uint8_t subpos = y; // Y is starting at 0 top left
+
+            if (color.is_on())
             {
-                this->write_byte(a_register); // Write register value to MAX
-                this->write_byte(data);       // Followed by actual data
+                this->led_displaybuffer_[subpos / 8][pos] |= (1 << subpos % 8);
             }
-            /*
+            else
+            {
+                this->led_displaybuffer_[subpos / 8][pos] &= ~(1 << subpos % 8);
+            }
+        }
+
+        void LedDisplayComponent::send_byte_(uint8_t a_register, uint8_t data)
+        {
+            this->write_byte(a_register); // Write register value to MAX
+            this->write_byte(data);       // Followed by actual data
+        }
+        /*
         void LedDisplayComponent::send_to_all_(uint8_t a_register, uint8_t data)
         {
             // this->enable();                                // Enable SPI
@@ -212,24 +213,24 @@ namespace esphome
                                                            // this->disable();                               // Disable SPI
         }
         */
-            void LedDisplayComponent::update()
+        void LedDisplayComponent::update()
+        {
+            this->update_ = true;
+            for (int chip_line = 0; chip_line < this->num_chip_lines_; chip_line++)
             {
-                this->update_ = true;
-                for (int chip_line = 0; chip_line < this->num_chip_lines_; chip_line++)
-                {
-                    this->led_displaybuffer_[chip_line].clear();
-                    this->led_displaybuffer_[chip_line].resize(get_width_internal(), this->bckgrnd_);
-                }
-                if (this->writer_local_.has_value()) // insert Labda function if available
-                    (*this->writer_local_)(*this);
+                this->led_displaybuffer_[chip_line].clear();
+                this->led_displaybuffer_[chip_line].resize(get_width_internal(), this->bckgrnd_);
             }
+            if (this->writer_local_.has_value()) // insert Labda function if available
+                (*this->writer_local_)(*this);
+        }
 
-            void LedDisplayComponent::invert_on_off(bool on_off) { this->invert_ = on_off; };
-            void LedDisplayComponent::invert_on_off() { this->invert_ = !this->invert_; };
+        void LedDisplayComponent::invert_on_off(bool on_off) { this->invert_ = on_off; };
+        void LedDisplayComponent::invert_on_off() { this->invert_ = !this->invert_; };
 
-            void LedDisplayComponent::turn_on_off(bool on_off)
-            {
-                /*
+        void LedDisplayComponent::turn_on_off(bool on_off)
+        {
+            /*
             if (on_off)
             {
                 this->send_to_all_(MAX7219_REGISTER_SHUTDOWN, 1);
@@ -239,155 +240,154 @@ namespace esphome
                 this->send_to_all_(MAX7219_REGISTER_SHUTDOWN, 0);
             }
             */
-            }
+        }
 
-            void LedDisplayComponent::scroll(bool on_off, ScrollMode mode, uint16_t speed, uint16_t delay, uint16_t dwell)
+        void LedDisplayComponent::scroll(bool on_off, ScrollMode mode, uint16_t speed, uint16_t delay, uint16_t dwell)
+        {
+            this->set_scroll(on_off);
+            this->set_scroll_mode(mode);
+            this->set_scroll_speed(speed);
+            this->set_scroll_dwell(dwell);
+            this->set_scroll_delay(delay);
+        }
+
+        void LedDisplayComponent::scroll(bool on_off, ScrollMode mode)
+        {
+            this->set_scroll(on_off);
+            this->set_scroll_mode(mode);
+        }
+
+        void LedDisplayComponent::scroll(bool on_off) { this->set_scroll(on_off); }
+
+        void LedDisplayComponent::scroll_left()
+        {
+            for (int chip_line = 0; chip_line < this->num_chip_lines_; chip_line++)
             {
-                this->set_scroll(on_off);
-                this->set_scroll_mode(mode);
-                this->set_scroll_speed(speed);
-                this->set_scroll_dwell(dwell);
-                this->set_scroll_delay(delay);
-            }
-
-            void LedDisplayComponent::scroll(bool on_off, ScrollMode mode)
-            {
-                this->set_scroll(on_off);
-                this->set_scroll_mode(mode);
-            }
-
-            void LedDisplayComponent::scroll(bool on_off) { this->set_scroll(on_off); }
-
-            void LedDisplayComponent::scroll_left()
-            {
-                for (int chip_line = 0; chip_line < this->num_chip_lines_; chip_line++)
+                if (this->update_)
                 {
-                    if (this->update_)
-                    {
-                        this->led_displaybuffer_[chip_line].push_back(this->bckgrnd_);
-                        for (uint16_t i = 0; i < this->stepsleft_; i++)
-                        {
-                            this->led_displaybuffer_[chip_line].push_back(this->led_displaybuffer_[chip_line].front());
-                            this->led_displaybuffer_[chip_line].erase(this->led_displaybuffer_[chip_line].begin());
-                        }
-                    }
-                    else
+                    this->led_displaybuffer_[chip_line].push_back(this->bckgrnd_);
+                    for (uint16_t i = 0; i < this->stepsleft_; i++)
                     {
                         this->led_displaybuffer_[chip_line].push_back(this->led_displaybuffer_[chip_line].front());
                         this->led_displaybuffer_[chip_line].erase(this->led_displaybuffer_[chip_line].begin());
                     }
                 }
-                this->update_ = false;
-                this->stepsleft_++;
+                else
+                {
+                    this->led_displaybuffer_[chip_line].push_back(this->led_displaybuffer_[chip_line].front());
+                    this->led_displaybuffer_[chip_line].erase(this->led_displaybuffer_[chip_line].begin());
+                }
             }
+            this->update_ = false;
+            this->stepsleft_++;
+        }
 
-            void LedDisplayComponent::send_char(uint8_t chip, uint8_t data)
-            {
-                // get this character from PROGMEM
-                for (uint8_t i = 0; i < 8; i++)
-                    this->led_displaybuffer_[0][chip * 8 + i] = progmem_read_byte(&MAX7219_DOT_MATRIX_FONT[data][i]);
-            } // end of send_char
+        void LedDisplayComponent::send_char(uint8_t chip, uint8_t data)
+        {
+            // get this character from PROGMEM
+            for (uint8_t i = 0; i < 8; i++)
+                this->led_displaybuffer_[0][chip * 8 + i] = progmem_read_byte(&MAX7219_DOT_MATRIX_FONT[data][i]);
+        } // end of send_char
 
-            // send one character (data) to position (chip)
+        // send one character (data) to position (chip)
 
-            void LedDisplayComponent::send64pixels(uint8_t chip, const uint8_t pixels[8])
-            {
-                for (uint8_t col = 0; col < 8; col++)
-                {                                      // RUN THIS LOOP 8 times until column is 7
-                                                       // this->enable();                    // start sending by enabling SPI
-                    for (uint8_t i = 0; i < chip; i++) // send extra NOPs to push the pixels out to extra displays
-                        this->send_byte_(MAX7219_REGISTER_NOOP,
-                                         MAX7219_REGISTER_NOOP); // run this loop unit the matching chip is reached
-                    uint8_t b = 0;                               // rotate pixels 90 degrees -- set byte to 0
-                    if (this->orientation_ == 0)
+        void LedDisplayComponent::send64pixels(uint8_t chip, const uint8_t pixels[8])
+        {
+            for (uint8_t col = 0; col < 8; col++)
+            {                                      // RUN THIS LOOP 8 times until column is 7
+                                                   // this->enable();                    // start sending by enabling SPI
+                for (uint8_t i = 0; i < chip; i++) // send extra NOPs to push the pixels out to extra displays
+                    this->send_byte_(MAX7219_REGISTER_NOOP,
+                                     MAX7219_REGISTER_NOOP); // run this loop unit the matching chip is reached
+                uint8_t b = 0;                               // rotate pixels 90 degrees -- set byte to 0
+                if (this->orientation_ == 0)
+                {
+                    for (uint8_t i = 0; i < 8; i++)
                     {
-                        for (uint8_t i = 0; i < 8; i++)
-                        {
-                            // run this loop 8 times for all the pixels[8] received
-                            b |= ((pixels[i] >> col) & 1) << (7 - i); // change the column bits into row bits
-                        }
+                        // run this loop 8 times for all the pixels[8] received
+                        b |= ((pixels[i] >> col) & 1) << (7 - i); // change the column bits into row bits
                     }
-                    else if (this->orientation_ == 1)
+                }
+                else if (this->orientation_ == 1)
+                {
+                    b = pixels[col];
+                }
+                else if (this->orientation_ == 2)
+                {
+                    for (uint8_t i = 0; i < 8; i++)
                     {
-                        b = pixels[col];
+                        b |= ((pixels[i] >> (7 - col)) & 1) << i;
                     }
-                    else if (this->orientation_ == 2)
-                    {
-                        for (uint8_t i = 0; i < 8; i++)
-                        {
-                            b |= ((pixels[i] >> (7 - col)) & 1) << i;
-                        }
-                    }
-                    else
-                    {
-                        b = pixels[7 - col];
-                    }
-                    // send this byte to display at selected chip
-                    if (this->invert_)
-                    {
-                        this->send_byte_(col + 1, ~b);
-                    }
-                    else
-                    {
-                        this->send_byte_(col + 1, b);
-                    }
-                    for (int i = 0; i < this->num_chips_ - chip - 1; i++) // end with enough NOPs so later chips don't update
-                        this->send_byte_(MAX7219_REGISTER_NOOP, MAX7219_REGISTER_NOOP);
-                    // this->disable(); // all done disable SPI
-                } // end of for each column
-            }     // end of send64pixels
+                }
+                else
+                {
+                    b = pixels[7 - col];
+                }
+                // send this byte to display at selected chip
+                if (this->invert_)
+                {
+                    this->send_byte_(col + 1, ~b);
+                }
+                else
+                {
+                    this->send_byte_(col + 1, b);
+                }
+                for (int i = 0; i < this->num_chips_ - chip - 1; i++) // end with enough NOPs so later chips don't update
+                    this->send_byte_(MAX7219_REGISTER_NOOP, MAX7219_REGISTER_NOOP);
+                // this->disable(); // all done disable SPI
+            } // end of for each column
+        }     // end of send64pixels
 
-            uint8_t LedDisplayComponent::printdigit(const char *str) { return this->printdigit(0, str); }
+        uint8_t LedDisplayComponent::printdigit(const char *str) { return this->printdigit(0, str); }
 
-            uint8_t LedDisplayComponent::printdigit(uint8_t start_pos, const char *s)
-            {
-                uint8_t chip = start_pos;
-                for (; chip < this->num_chips_ && *s; chip++)
-                    send_char(chip, *s++);
-                // space out rest
-                while (chip < (this->num_chips_))
-                    send_char(chip++, ' ');
-                return 0;
-            } // end of sendString
+        uint8_t LedDisplayComponent::printdigit(uint8_t start_pos, const char *s)
+        {
+            uint8_t chip = start_pos;
+            for (; chip < this->num_chips_ && *s; chip++)
+                send_char(chip, *s++);
+            // space out rest
+            while (chip < (this->num_chips_))
+                send_char(chip++, ' ');
+            return 0;
+        } // end of sendString
 
-            uint8_t LedDisplayComponent::printdigitf(uint8_t pos, const char *format, ...)
-            {
-                va_list arg;
-                va_start(arg, format);
-                char buffer[64];
-                int ret = vsnprintf(buffer, sizeof(buffer), format, arg);
-                va_end(arg);
-                if (ret > 0)
-                    return this->printdigit(pos, buffer);
-                return 0;
-            }
-            uint8_t LedDisplayComponent::printdigitf(const char *format, ...)
-            {
-                va_list arg;
-                va_start(arg, format);
-                char buffer[64];
-                int ret = vsnprintf(buffer, sizeof(buffer), format, arg);
-                va_end(arg);
-                if (ret > 0)
-                    return this->printdigit(buffer);
-                return 0;
-            }
+        uint8_t LedDisplayComponent::printdigitf(uint8_t pos, const char *format, ...)
+        {
+            va_list arg;
+            va_start(arg, format);
+            char buffer[64];
+            int ret = vsnprintf(buffer, sizeof(buffer), format, arg);
+            va_end(arg);
+            if (ret > 0)
+                return this->printdigit(pos, buffer);
+            return 0;
+        }
+        uint8_t LedDisplayComponent::printdigitf(const char *format, ...)
+        {
+            va_list arg;
+            va_start(arg, format);
+            char buffer[64];
+            int ret = vsnprintf(buffer, sizeof(buffer), format, arg);
+            va_end(arg);
+            if (ret > 0)
+                return this->printdigit(buffer);
+            return 0;
+        }
 
 #ifdef USE_TIME
-            uint8_t LedDisplayComponent::strftimedigit(uint8_t pos, const char *format, time::ESPTime time)
-            {
-                char buffer[64];
-                size_t ret = time.strftime(buffer, sizeof(buffer), format);
-                if (ret > 0)
-                    return this->printdigit(pos, buffer);
-                return 0;
-            }
-            uint8_t LedDisplayComponent::strftimedigit(const char *format, time::ESPTime time)
-            {
-                return this->strftimedigit(0, format, time);
-            }
+        uint8_t LedDisplayComponent::strftimedigit(uint8_t pos, const char *format, time::ESPTime time)
+        {
+            char buffer[64];
+            size_t ret = time.strftime(buffer, sizeof(buffer), format);
+            if (ret > 0)
+                return this->printdigit(pos, buffer);
+            return 0;
+        }
+        uint8_t LedDisplayComponent::strftimedigit(const char *format, time::ESPTime time)
+        {
+            return this->strftimedigit(0, format, time);
+        }
 #endif
 
-        } // namespace LedDisplay_ns
-    }     // namespace esphome
-}
+    } // namespace LedDisplay_ns
+} // namespace esphome
